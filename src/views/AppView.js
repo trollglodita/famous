73f6cf6894f9a4df58e31f70bc1b78e033e96c59
Trollglodita <1trollglodita@gmail.com>
2014-09-05 13:08:23 -0400

    Adding the PageView

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -4,14 +4,25 @@ define(function(require, exports, module) {
     var Transform     = require('famous/core/Transform');
     var StateModifier = require('famous/modifiers/StateModifier');
 
+    var PageView = require('views/PageView');
+
     function AppView() {
         View.apply(this, arguments);
+
+         _createPageView.call(this);
     }
 
     AppView.prototype = Object.create(View.prototype);
     AppView.prototype.constructor = AppView;
 
     AppView.DEFAULT_OPTIONS = {};
+
+    function _createPageView() {
+        this.pageView = new PageView();
+        this.pageModifier = new StateModifier();
+
+        this.add(this.pageModifier).add(this.pageView);
+    }
 
     module.exports = AppView;
 });
