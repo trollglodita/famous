df1758a047f39b1fdfff8eadf2fbbacc7f74005a
Trollglodita <1trollglodita@gmail.com>
2014-09-05 13:11:38 -0400

    Creating the Layout

--- src/views/PageView.js
+++ src/views/PageView.js
@@ -3,15 +3,32 @@ define(function(require, exports, module) {
     var Surface = require('famous/core/Surface');
     var Transform = require('famous/core/Transform');
     var StateModifier = require('famous/modifiers/StateModifier');
+    var HeaderFooter    = require('famous/views/HeaderFooterLayout');
 
     function PageView() {
         View.apply(this, arguments);
+
+        _createLayout.call(this);
     }
 
     PageView.prototype = Object.create(View.prototype);
     PageView.prototype.constructor = PageView;
 
-    PageView.DEFAULT_OPTIONS = {};
+    PageView.DEFAULT_OPTIONS = {
+        headerSize: 44
+    };
+
+    function _createLayout() {
+        this.layout = new HeaderFooter({
+            headerSize: this.options.headerSize
+        });
+
+        var layoutModifier = new StateModifier({
+            transform: Transform.translate(0, 0, 0.1)
+        });
+
+        this.add(layoutModifier).add(this.layout);
+    }
 
     module.exports = PageView;
 });
